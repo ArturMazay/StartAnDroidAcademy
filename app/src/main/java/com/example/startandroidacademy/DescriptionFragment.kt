2ec73e8b5054842507cbbe13d6329af3049b4b92
mazay1986 <60013249+mazay1986@users.noreply.github.com>
2020-11-25 10:17:57 +0200

    домашнее задание отрисовал айтем,заменил на фрагменты
    Signed-off-by: mazay1986 <60013249+mazay1986@users.noreply.github.com>

--- app/src/main/java/com/example/startandroidacademy/DescriptionFragment.kt
+++ app/src/main/java/com/example/startandroidacademy/DescriptionFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.example.startandroidacademy.databinding.FragmentTitleBinding
 
 
 class DescriptionFragment : Fragment() {
@@ -14,8 +16,13 @@ class DescriptionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_description, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title,
+            container,
+            false
+        )
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/startandroidacademy/TitleFragment.kt
+++ app/src/main/java/com/example/startandroidacademy/TitleFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -13,15 +12,17 @@ import com.example.startandroidacademy.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title,
+            container,
+            false
+        )
 
-    //Inflating and Returning the View with DataBindingUtil
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
-
-        binding.button.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_titleFragment_to_descriptionFragment)
-        }
 
         return binding.root
     }
