68665e587ca0c4b24d7cfcd1b02af2772b569c7c
mazay1986 <60013249+mazay1986@users.noreply.github.com>
2020-11-27 11:07:19 +0200

    домашнее задание разобрался с методами фрагментменеджера
    Signed-off-by: mazay1986 <60013249+mazay1986@users.noreply.github.com>

--- app/src/main/java/com/example/startandroidacademy/MainActivity.kt
+++ app/src/main/java/com/example/startandroidacademy/MainActivity.kt
@@ -1,27 +1,23 @@
 package com.example.startandroidacademy
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(),OnClickListenerToMovieDetails {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val button : Button = findViewById(R.id.button)
-
-
-        button.setOnClickListener {
-            val intent = Intent (this@MainActivity,MovieDetailsActivity::class.java)
-            startActivity(intent)
-
-        }
-
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main_container, TitleMovieFragment())
+            .commit()
+    }
+//не работает
+    override fun onClickOpenDetailsMovieFragment() {
+       supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container,DetailsMovieFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/startandroidacademy/TitleMovieFragment.kt
@@ -0,0 +1,46 @@
+package com.example.startandroidacademy
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+
+class TitleMovieFragment : Fragment() {
+
+    private var onClickListenerToMovieDetails:OnClickListenerToMovieDetails? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_title_movie, container, false)
+        val button:Button=view.findViewById(R.id.button_open)
+        button.setOnClickListener {
+            onClickListenerToMovieDetails?.onClickOpenDetailsMovieFragment()
+            //Log.e("XXX",("WORK GOOD").toString())
+        }
+
+
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+            onClickListenerToMovieDetails = context as? OnClickListenerToMovieDetails
+           // Log.e("XXX",(onClickListenerToMovieDetails==null).toString())
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        onClickListenerToMovieDetails = null
+    }
+
+    companion object {
+
+    }
+}
