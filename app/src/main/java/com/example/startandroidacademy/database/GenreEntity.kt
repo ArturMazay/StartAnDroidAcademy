fb10303b47eff7240733ebf10298b4a0009d3a6c
mazay1986 <60013249+mazay1986@users.noreply.github.com>
2021-02-08 09:31:51 +0200

    ActorEntity.kt+ GenreEntity.kt+

--- app/src/main/java/com/example/startandroidacademy/database/GenreEntity.kt
+++ app/src/main/java/com/example/startandroidacademy/database/GenreEntity.kt
@@ -3,22 +3,21 @@ package com.example.startandroidacademy.database
 import androidx.room.*
 
 @Entity(
-    tableName = "actor",
+    tableName = "genre",
     foreignKeys = [ForeignKey(
         entity = MovieEntity::class,
-        parentColumns = ["actor_id"],
+        parentColumns = ["genre_id"],
         childColumns = ["movie_id"],
         onDelete = ForeignKey.CASCADE
     )], indices = [Index(value = ["movie_id"])]
 )
-data class ActorEntity(
+class GenreEntity(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "actor_id")
-    val actor_id: Int,
-    @ColumnInfo(name = "name")
-    val name: String,
-    @ColumnInfo(name = "picture_actor")
-    val picture: String,
+    @ColumnInfo(name = "genre_id")
+    val genre_id: Long,
     @ColumnInfo(name = "movie_id")
-    val movie_id: Int
-)
+    val movie_id: Long,
+    @ColumnInfo(name = "name_genres")
+    val name_genres: String
+) {
+}
